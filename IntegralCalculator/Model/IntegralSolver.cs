3b9cc87a8aefd090393b3ec7684806b0bccc57e5
Yehor <mr.robik12345@gmail.com>
2024-05-19 16:03:40 +0300

    Init

---
+++ IntegralCalculator/Model/IntegralSolver.cs
@@ -0,0 +1,40 @@
+namespace IntegralCalculator.Model;
+
+public class IntegralSolver
+{
+    // public Dictionary<string, string> operations = new Dictionary<string, string>()
+    // {
+    //     {"sin(.)", "\\sin{.}"},
+    //     {"./.", "\\frac{.}{.}"},
+    //     {"Surd[.,.]", " "},
+    //     {"Surd[.,.]", " "},
+    //     {"Surd[.,.]", " "},
+    //     {"Surd[.,.]", " "},
+    //     {"Surd[.,.]", " "},
+    //     {"Surd[.,.]", " "},
+    //     {"Surd[.,.]", " "},
+    //     {"Surd[.,.]", " "},
+    //     {"Surd[.,.]", " "},
+    //     {"Surd[.,.]", " "},
+    //     {"Surd[.,.]", " "},
+    // };
+    
+    
+    
+    public static void SolveByRectangles()
+    {
+        string function;
+        string upperBound;
+        string lowerBound;
+    }
+    
+    public static void SolveBySipmpson()
+    {
+        
+    }
+    
+    public static void SolveByTrapezium()
+    {
+        
+    }
+}
